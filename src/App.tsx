7159453f406bac66889cf56ac9706b4b38f13e2b
Naman Attri <namanattri5@gmail.com>
2023-03-11 13:45:48 +0530

    Creates a 15x15 grid for ludo board

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const cellHeight = 40;
+  const cellWidth = 40;
+  const cellRowCount = 15;
+  const cellColCount = 15;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Board" style={{
+          float: "left",
+        }}>
+          {
+            Array.from({length: cellRowCount}, (_, i) => i + 1).map(i =>
+              <div key={i}>
+                {Array.from({length: cellColCount}, (_, j) => j + 1).map(j => 
+                  <div key={j} style={{
+                    border: "1px solid black",
+                    width: `${cellWidth}px`,
+                    height: `${cellHeight}px`,
+                    float: "left",
+                  }}></div>
+                )}
+              </div>
+            )
+          }
+        </div>
       </header>
     </div>
   );
