bf670900b53c31f19686fbb303b374e238d71d02
Naman Attri <namanattri5@gmail.com>
2023-03-11 13:52:57 +0530

    Moves the grid maker to board component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import React from 'react';
+import React from 'react'
+import Board from './components/Board'
 
 function App() {
-  const cellHeight = 40;
-  const cellWidth = 40;
-  const cellRowCount = 15;
-  const cellColCount = 15;
-
   return (
     <div className="App">
       <header className="App-header">
-        <div className="Board" style={{
-          float: "left",
-        }}>
-          {
-            Array.from({length: cellRowCount}, (_, i) => i + 1).map(i =>
-              <div key={i}>
-                {Array.from({length: cellColCount}, (_, j) => j + 1).map(j => 
-                  <div key={j} style={{
-                    border: "1px solid black",
-                    width: `${cellWidth}px`,
-                    height: `${cellHeight}px`,
-                    float: "left",
-                  }}></div>
-                )}
-              </div>
-            )
-          }
-        </div>
+        <Board />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
